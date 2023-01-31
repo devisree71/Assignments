d15d74aa0a1302beef72f46f42de6fa6153832bb
Devi <kolluridevisree@gmail.com>
2023-01-31 18:34:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./styles.css";
+import imgg from "./Images/lizard.jpg";
+function Last(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <img src={imgg} className="img"></img>
+      <h2>Lizard</h2>
+      <p>
+        Lizards are widespread group of squamate reptiles,with over 6,000
+        species,ranging across all continents except Antarctica
+      </p>
+      <a href="#" class="link">
+        SHARE
+      </a>
+      <a href="#" class="link">
+        LEARN MORE
+      </a>
     </div>
   );
 }
 
-export default App;
+export default Last;
